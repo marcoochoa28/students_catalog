3ffc047cd154814809685a203822784198730424
Antonio Chavez <cavjzz@gmail.com>
2015-12-01 08:26:03 -0600

    Add students resource

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,10 @@
 class Student < ActiveRecord::Base
+  validates :first_name, :last_name, :birthdate,
+    :control_number, :semester, presence: true
+
+  validates :first_name, uniqueness: { scope: [ :last_name, :birthdate ] }
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
